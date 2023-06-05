7dfbca425aee082d0fd28ed20e159429b9208baf
Yuhsuanlinn <99881755+Yuhsuanlinn@users.noreply.github.com>
2023-06-06 02:26:53 +0800

    Update and rename SGBM/program to SGBM/project/args.mk

---
+++ SGBM/project/args.mk
@@ -0,0 +1,7 @@
+INPUT_IMG_0 	:= /home/hls02/sgbm-new/dataset/sequences/00/image_0/
+INPUT_IMG_1 	:= /home/hls02/sgbm-new/dataset/sequences/00/image_1/
+INPUT_ARG 		:= /home/hls02/sgbm-new/dataset/sequences/00/calib.txt
+Frames_Num 		:= 3
+PLATFORM_VENDOR := Xilinx
+HW_PLATFORM 	:= xilinx_u50_gen3x16_xdma_base_5
+XCLBIN_NAME		:= top_function.xclbin
